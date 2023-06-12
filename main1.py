f94b750c214718b90f2e7069dd91184615d7b4bf
JJJJJJJJJoy <128284420+JJJJJJJJJoy@users.noreply.github.com>
2023-06-12 23:06:07 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -1,3 +1,12 @@
-q = input()
-print(q)
+class Solution:
+   def twoSum(self, nums: List[int], target: int) -> List[int]:
+       
+      dic = {}
+      for i , value in enumerate(nums):
+          r = target - nums[i]
+
+          if r in dic:
+              return[i, dic[r]]
+
+          dic[value] = i
 
